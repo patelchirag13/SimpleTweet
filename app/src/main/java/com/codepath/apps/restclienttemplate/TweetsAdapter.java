ae13c1a46b38bb4d1230b2fcaa75351793ec253c
patelchirag13 <cyp5@njit.edu>
2021-09-29 21:26:27 -0400

    Added pull down to refresh feature

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -48,7 +48,20 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     public int getItemCount() {
         return tweets.size();
     }
-    // Define a viewholder
+
+    // Clean all elements of the recycler
+    public void clear(){
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items
+    public void addAll(List<Tweet> tweetList){
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    // Define a view holder
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         ImageView ivProfileImage;
